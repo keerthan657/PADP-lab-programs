1087e7af966d7ec97bce2fb73fce7eed9f527d0e
Keerthan Kumar <keerthankk212001@gmail.com>
2023-02-15 07:42:08 +0530

    Updated code

--- montecarlo_mpi.c
+++ montecarlo_mpi.c
@@ -7,20 +7,26 @@ int main(int argc, char *argv[])
     MPI_Init(&argc, &argv);
 
     int rank, size;
-    MPI_Comm_Rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_Size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int sum=0, count=0, n=1000000;
-    for(int i=0; i<n; i++)
+    long long int n = 1000000;
+    int localCount = 0, count = 0;
+    double x, y;
+    for(long long int i=0; i<n; i++)
     {
         x = (double)rand()/RAND_MAX;
         y = (double)rand()/RAND_MAX;
-        if(x*x + y*y < 1)
-            count++;
+        if((x*x + y*y) < 1)
+            localCount++;
     }
 
-    MPI_Reduce(&count, &sum, 0, MPI_INT, MPI_SUM, 1, MPI_COMM_WORLD);
+    MPI_Reduce(&localCount, &count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    printf("value of pi=%lf\n", 4*sum/(double)n);
+    if(rank==0)
+        printf("value of pi: %lf\n", (4*count/(double)(n*size)));
+
+    MPI_Finalize();
+
     return 0;
 }
